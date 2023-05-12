49d0331332d8e74c0130bc938d925beb4a587813
Simon Andreas Bjørn <simon.bjorn@ellipticlabs.com>
2023-05-12 14:00:34 +0200

    Update config

--- mappings.lua
+++ mappings.lua
@@ -11,7 +11,10 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
+          require("astronvim.utils.buffer").close(
+            bufnr)
+        end)
       end,
       desc = "Pick to close",
     },
@@ -19,10 +22,30 @@ return {
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
+
+    -- Tab commands
+    ["<leader>Tn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+    ["<leader>Tc"] = { "<cmd>tabclose<cr>", desc = "Close tab" },
+    ["<leader>T"] = { name = "Tab" },
+
+    -- Dainou remaps
+    ["J"] = { "mzJ`z" },
+    ["<C-u>"] = { "<C-u>zz" }, -- Reset cursor position after scroll
+    ["<C-d>"] = { "<C-d>zz" }, -- Reset cursor position after scroll
+
+    ["<leader>y"] = { "\"+y", desc = "Yank to clipboard" }
+    ["<leader>Y"] = { "\"+Y", desc = "Yank line to clipboard" }
+
+
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
+  },
+  v = {
+    -- Move visual block up and down
+    ["K"] = { ":m '<-2<CR>gv=gv", desc = "Move visual up" },
+    ["J"] = { ":m '>+1<CR>gv=gv", desc = "Move visual down" },
   },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,13 @@
 return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    config = function()
+      require("catppuccin").setup {}
+    end
+  },
+  --
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
